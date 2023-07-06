340bf0f916fe33a5dee10631a3261252ef6c8123
Abhinav <ab935176@dal.ca>
2023-07-06 11:50:38 -0300

    v a changes

--- backend/microservices/virtual-assistance/index.js
+++ backend/microservices/virtual-assistance/index.js
@@ -1,13 +1,45 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const express = require("express");
+const serverless = require("serverless-http");
+const chatBotRouter = require('./routes/chatbot.route');
+
+const app = express();
+app.use(express.json());
+app.use('/api/chatBot', chatBotRouter);
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// index.js
+
+const ChatBotController = require('./controllers/chatbot.controller');
+
+module.exports.handler = async (event, context) => {
+    console.log(`Received event from Lex: ${JSON.stringify(event)}`);
+
+    const currentIntent = event.currentIntent.name;
+
+    switch (currentIntent) {
+        case 'NavigationHelpIntent':
+            return await ChatBotController.getNavigationHelp(event);
+        case 'ScoreInquiryIntent':
+            return await ChatBotController.getScoreInquiry(event, event.currentIntent.slots.TeamName);
+        default:
+            console.log(`Received unknown intent: ${currentIntent}`);
+            return {
+                sessionAttributes: {},
+                dialogAction: {
+                    type: 'ElicitIntent',
+                    message: {
+                        contentType: 'PlainText',
+                        content: 'Sorry, I did not understand. Can you please rephrase?',
+                    },
+                },
+            };
+    }
 };
+
+
+
+module.exports.handler = serverless(app);
---
+++ backend/microservices/virtual-assistance/routes/chatbot.route.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const ChatBotController = require('../controllers/chatbot.controller');
+// // router.post('/message', ChatBotController.sendMessageToLex);
+// router.get('/navigation', ChatBotController.getNavigationHelp);
+// router.get('/teamscore/:name', ChatBotController.getScoreInquiry);
+
+
+// module.exports = router;
