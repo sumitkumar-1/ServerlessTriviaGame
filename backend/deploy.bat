1efd1528b5bd9838e2433667f51523aefc513681
Sumit Kumar <sm585558@dal.ca>
2023-08-03 13:39:09 -0300

    update deployment script

--- backend/deploy.bat
+++ backend/deploy.bat
@@ -1,15 +1,18 @@
 @echo off
-
 setlocal
 
 set "workDir=%cd%"
 
 echo Starting to deploy microservices
 
-for /d %%i in (%workDir%\microservices\*) do (
-    echo deploying microservice: %%~nxi
-    cd "%%i"
-    serverless deploy
+for /d %%i in ("%workDir%\microservices\*") do (
+    echo Deploying microservice: %%~nxi
+    if exist "%%i\serverless.yml" (
+        cd "%%i"
+        serverless deploy
+    ) else (
+        echo serverless.yml not found in %%~nxi. Skipping deployment.
+    )
 )
 
 echo Completed deployment of microservices
